66c599684b324edebafb3e0c38c05afa2ca2ba12
Pavel Fanaskov <paul-fanaskov@yandex-team.ru>
2020-12-07 10:40:40 +0300

    remove unused import from menu screen

--- screens/menu/menu.tsx
+++ screens/menu/menu.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, FlatList, Text, StyleSheet} from 'react-native'
+import {View, StyleSheet} from 'react-native'
 import {ScrollView} from 'react-native-gesture-handler'
 import {Screen} from '../../common/layout/Screen'
 import {Header} from '../../common/layout/header'
